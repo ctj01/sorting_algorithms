50b092d9d70a26645a5604417f8bf9e1af2bb049
ctj01 <flexdancer507@gmail.com>
2021-02-02 21:10:14 -0500

    quick sort

--- sort.h
+++ sort.h
@@ -26,5 +26,8 @@ void merge_sort(int *array, size_t size);
 void rem(int *array, int *aux, size_t i, size_t j, size_t c, size_t size);
 void selection_sort(int *array, size_t size);
 void swap(int *aux, int *array);
+print_sort(int array[], size_t size, int init);
+size_t partition(int array[], size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
